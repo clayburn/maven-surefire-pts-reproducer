e10f08c1c0d3d3ae74c0518def3260308c8751f1
Clay Johnson <cjohnson@gradle.com>
2023-03-17 12:19:30 -0500

    Add PTS

---
+++ README.md
@@ -0,0 +1,6 @@
+To reproduce:
+
+> ./mvnw clean test
+> ./mvnw clean test -DisPTSEnabled=true
+
+With PTS enabled, we see this failure: https://ge.solutions-team.gradle.com/s/r2yoqtu3hnczs/failure#1
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,14 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest
 {
-    /**
-     * Rigourous Test :-)
-     */
+
     @Test
     public void testApp()
     {
